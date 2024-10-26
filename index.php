b478312fdd858e52f4cee3ede8bd88050f1e5083
Dev kumar <kumarravi91158@gmail.com>
2024-10-26 11:01:37 +0530

    .

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!-- <?php
+ECHO "Hello World!<br>";
+echo"hi bro";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order a burger</button>
+</body>
+</html> -->
+
+
+<?php 
+     $name = "dev kumar";
+     $email = "vhjewvkb@gamil.com";
+     echo "Hello {$name}<br>";
+     echo "You email is : {$email}";
+     
+
+?>
